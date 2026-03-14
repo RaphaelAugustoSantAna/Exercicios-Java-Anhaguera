1068daf3fe640eb5c33d83d00aa779096f78e20e
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2026-03-14 16:24:05 -0300

    executando programa para encontrar qual número é positivo e negativo utilizando java

---
+++ Decisao.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Decisao {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+    System.out.print("Entre com um valor numérico..: ");
+        int numero = s.nextInt();
+
+        if (numero > 0) {
+            System.out.println("O número é positivo: " + numero);
+        } else if (numero < 0) {
+            System.out.println("O número é negativo: " + numero);
+        } else {
+            System.out.println("O número é igual a 0(zero)" + numero);
+        }
+
+        System.out.println("Fim do programa");
+    }
+
+}
