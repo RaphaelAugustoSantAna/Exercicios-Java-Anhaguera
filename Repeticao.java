bf563ec831e75b9188ca840dc8d06fdcd92edb4e
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2026-03-17 14:34:45 -0300

    comando while

---
+++ Repeticao.java
@@ -0,0 +1,14 @@
+public class Repeticao {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 10) {
+
+            System.out.println("** Vai Corinthians **");
+            i++;
+        }
+
+        System.out.println("Fim do Programa!");
+    }
+}
