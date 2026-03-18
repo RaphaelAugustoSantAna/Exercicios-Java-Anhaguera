f0269e982cbe9494b976d151f7934c77715d650a
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2026-03-18 20:32:33 -0300

    iniciando exercícios sobre Definição e uso de Classes em Java

---
+++ Aluno.java
@@ -0,0 +1,36 @@
+public class Aluno {
+    private String nome;
+    private int matricula;
+    private double nota;
+
+    public Aluno(String nome, int matricula, double nota) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+
+    // métodos get e set para encapsulamento
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+     public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+     public void setNota(double nota) {
+        this.nota = nota;
+    }
+}
