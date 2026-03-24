1035f2af14b07896c9d38ee7e76d7012281799e0
Raphael Sant'Ana <raphael_a_santana@outlook.com>
2026-03-23 21:46:46 -0300

    finalizando exercício com uso de Interfaces, utilizando também Herança e polimorfismo

--- Escola.java
+++ Escola.java
@@ -4,5 +4,12 @@ public class Escola {
         aluno1.setNota(10);
 
         aluno1.exibirInformacoes();
+        System.out.println("*********************************");
+
+        Avaliacao alunoGrad = new AlunoGraduacao("Isaac", 2, 7);
+        Avaliacao alunoPos = new AlunoPosGraduacao("Francisco", 3, 7);
+
+        System.out.println("Média do Isaac (Graduação)..:" + alunoGrad.calcularMedia());
+        System.out.println("Média do Francisco (pós-graduação)..:" + alunoPos.calcularMedia());
     }
 }
